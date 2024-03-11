df04eafe20a80f68123515b90f2bcc0767f07469
Lukas Scholz <l.kas.scholz@gmail.com>
2024-03-11 21:31:35 +0100

    Update model.py

--- nanoGPT/model.py
+++ nanoGPT/model.py
@@ -65,7 +65,7 @@ class CausalSelfAttention(nn.Module):
         else:
             # manual implementation of attention
             att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-            att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+            att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf')) #TODO comment out to encode the future
             att = F.softmax(att, dim=-1)
             att = self.attn_dropout(att)
             y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
